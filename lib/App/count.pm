85fa930e8b81f015987811d5b7d658fe4702c00b
Yasutaka ATARASHI (新 康孝) <yakex@cpan.org>
2013-05-02 23:55:25 +0000

    Import of YAKEX/App-count-v0.0.1 from CPAN.
    gitpan-cpan-distribution: App-count
    gitpan-cpan-version:      v0.0.1
    gitpan-cpan-path:         YAKEX/App-count-v0.0.1.tar.gz
    gitpan-cpan-author:       YAKEX
    gitpan-cpan-maturity:     released

---
+++ lib/App/count.pm
@@ -0,0 +1,143 @@
+package App::count;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Counting utility for a file consisting of the fixed number of fields like CSV
+our $VERSION = 'v0.0.1'; # VERSION
+
+use Getopt::Long qw(GetOptionsFromArray);
+use Pod::Usage;
+use YAML::Any;
+
+Getopt::Long::Configure('posix_default', 'no_ignore_case');
+
+sub run
+{
+	shift if @_ && eval { $_[0]->isa(__PACKAGE__) };
+	my @spec;
+	my $handler = sub { my $key = $_[0]; push @spec, map { [$key, $_-1 ] } split /,/, $_[1]; };
+	my %opts = (
+		c => sub { push @spec, ['count']; },
+		sum => $handler, max => $handler, min => $handler, avg => $handler,
+		'map' => sub {
+			my @t = split /,/, $_[1];
+			while(my ($idx, $key) = splice(@t, 0, 2)) {
+				push @spec, ['map', $idx-1, $key];
+			}
+		}
+	);
+	GetOptionsFromArray(\@_, \%opts,
+		'g|group=s@', 'c|count', 'sum|s=s@', 'map|m=s@', 'M|map-file=s', 't|delimiter=s',
+		'max=s@', 'min=s@', 'avg|ave=s@',
+	);
+
+	my $map;
+	$map = YAML::Any::LoadFile($opts{M}) or die "Can't load map file" if exists $opts{M};
+	my $group = exists $opts{g} ? [map { $_ -1 } map { split /,/ } @{$opts{g}}] : undef;
+	push @spec, ['count'] if ! @spec;
+	my $odelimiter = $opts{t} || "\t";
+	$opts{t} ||= '\s+';
+
+	my %init = (
+		max => sub { undef },
+		min => sub { undef },
+		avg => sub { [0,0] }, # Return new array reference
+		sum => sub { 0 },
+		count => sub { 0 },
+		'map' => sub { undef },
+	);
+
+	push @_, '-' if ! @_;
+	while(my $file = shift @_) {
+		my $fh;
+		if($file ne '-') {
+			open $fh, '<', $file;
+		} else {
+			$fh = \*STDIN;
+		}
+
+		my %data;
+		my %proc = ( # $key, $idx, \@F
+			max   => sub { my ($key, $idx, $F) = @_; $data{$key}[$idx] = $F->[$spec[$idx][1]] if ! defined $data{$key}[$idx] || $data{$key}[$idx] < $F->[$spec[$idx][1]]; },
+			min   => sub { my ($key, $idx, $F) = @_; $data{$key}[$idx] = $F->[$spec[$idx][1]] if ! defined $data{$key}[$idx] || $data{$key}[$idx] > $F->[$spec[$idx][1]]; },
+			avg   => sub { my ($key, $idx, $F) = @_; ++$data{$key}[$idx][0]; $data{$key}[$idx][1] += $F->[$spec[$idx][1]]; },
+			sum   => sub { my ($key, $idx, $F) = @_; $data{$key}[$idx] += $F->[$spec[$idx][1]]; },
+			count => sub { my ($key, $idx, $F) = @_; ++$data{$key}[$idx]; },
+			'map' => sub { my ($key, $idx, $F) = @_; $data{$key}[$idx] ||= $map->{$spec[$idx][2]}{$F->[$spec[$idx][1]]}; },
+		);
+		while(<$fh>) {
+			s/[\r\n]+$//;
+			my @F = split /$opts{t}/;
+
+			my $key = defined $group ? join("\x00", @F[@$group]) : '_';
+
+			foreach my $idx (0..$#spec) {
+				$data{$key}[$idx] ||= $init{$spec[$idx][0]}->();
+				$proc{$spec[$idx][0]}->($key, $idx, \@F);
+			}
+		}
+
+		if($file ne '-') {
+			close $fh;
+		}
+
+		foreach my $key (sort keys %data) {
+			my @F;
+			push @F, split /\x00/, $key if exists $opts{g};
+			push @F, map { ref $_ ? $_->[1]/$_->[0] : $_ } @{$data{$key}};
+			print join($odelimiter, @F), "\n";
+		}
+	}
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::count - Counting utility for a file consisting of the fixed number of fields like CSV
+
+=head1 VERSION
+
+version v0.0.1
+
+=head1 SYNOPSIS
+
+  App::count->run(@ARGV);
+
+=head1 DESCRIPTION
+
+This is an implementation module of a counting utility for a file consisting of the fixed number of fields.
+
+=head1 METHODS
+
+=head2 C<run(@arg)>
+
+Process arguments. Typically, C<@ARGV> is passed. For argument details, see L<installdeps>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+L<count>
+
+=back
+
+=head1 AUTHOR
+
+Yasutaka ATARASHI <yakex@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Yasutaka ATARASHI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,226 @@
+use Test::More;
+use App::count;
+
+sub check
+{
+	pipe(FROM_PARENT, TO_CHILD);
+	pipe(FROM_CHILD, TO_PARENT);
+	my $pid = fork;
+	die "fork failed: $!" if ! defined($pid);
+	if($pid) {
+		close FROM_PARENT;
+		close TO_PARENT;
+		print TO_CHILD $_[1];
+		close TO_CHILD;
+		local $/ = undef;
+		my $output = <FROM_CHILD>;
+		close FROM_CHILD;
+		is($output, $_[2], $_[3]);
+		waitpid $pid, 0;
+	} else {
+		close FROM_CHILD;
+		close TO_CHILD;
+		open STDIN, "<&FROM_PARENT";
+		open STDOUT, ">&TO_PARENT";
+		App::count->run(@{$_[0]});
+		close FROM_PARENT;
+		close TO_PARENT;
+		exit;
+	}
+}
+
+my @tests = (
+	[
+		[], <<EOF,
+1	2	3
+1	1	1
+2	3	1
+3	1	2
+EOF
+		<<EOF,
+4
+EOF
+		'no argument'
+	],
+	[
+		[qw(-g 1)], <<EOF,
+1	2	3
+1	1	1
+2	3	1
+3	1	2
+EOF
+		<<EOF,
+1	2
+2	1
+3	1
+EOF
+		'-g'
+	],
+	[
+		[qw(-g 1 -c -s 2,3)], <<EOF,
+1	2	3
+1	1	1
+2	3	1
+3	1	2
+EOF
+		<<EOF,
+1	2	3	4
+2	1	3	1
+3	1	1	2
+EOF
+		'-gcs,'
+	],
+	[
+		[qw(-g 1 -c -s 2 -s 3)], <<EOF,
+1	2	3
+1	1	1
+2	3	1
+3	1	2
+EOF
+		<<EOF,
+1	2	3	4
+2	1	3	1
+3	1	1	2
+EOF
+		'-gcss'
+	],
+	[
+		[qw(-g 1,2)], <<EOF,
+1	2	3
+1	2	4
+1	1	1
+2	3	1
+3	1	2
+1	1	2
+2	3	2
+EOF
+		<<EOF,
+1	1	2
+1	2	2
+2	3	2
+3	1	1
+EOF
+		'-g,'
+	],
+	[
+		[qw(-g 1 -g 2)], <<EOF,
+1	2	3
+1	2	4
+1	1	1
+2	3	1
+3	1	2
+1	1	2
+2	3	2
+EOF
+		<<EOF,
+1	1	2
+1	2	2
+2	3	2
+3	1	1
+EOF
+		'-gg'
+	],
+	[
+		[qw(-g 2 -g 1)], <<EOF,
+1	2	3
+1	2	4
+1	1	1
+2	3	1
+3	1	2
+1	1	2
+2	3	2
+EOF
+		<<EOF,
+1	1	2
+1	3	1
+2	1	2
+3	2	2
+EOF
+		'-gg rev'
+	],
+	[
+		[qw(-g 2 -g 1 -t ,)], <<EOF,
+1,2,3
+1,2,4
+1,1,1
+2,3,1
+3,1,2
+1,1,2
+2,3,2
+EOF
+		<<EOF,
+1,1,2
+1,3,1
+2,1,2
+3,2,2
+EOF
+		'-ggt rev'
+	],
+	[
+		[qw(-g 1 -s 2 -s 3 -c)], <<EOF,
+1	2	3
+1	1	1
+2	3	1
+3	1	2
+EOF
+		<<EOF,
+1	3	4	2
+2	3	1	1
+3	1	2	1
+EOF
+		'-gssc'
+	],
+	[
+		[qw(-g 1 -s 2 --max 3 --min 3 --avg 3)], <<EOF,
+1	2	3
+1	1	1
+2	3	1
+3	1	2
+EOF
+		<<EOF,
+1	3	3	1	2
+2	3	1	1	1
+3	1	2	2	2
+EOF
+		'max, min, avg'
+	],
+	[
+		[qw(-g 1,2 -m 1,field1 -m 2,field2 -c -M t/map.yaml)], <<EOF,
+1	2	3
+1	2	4
+1	1	1
+2	3	1
+3	1	2
+1	1	2
+2	3	2
+EOF
+		<<EOF,
+1	1	one	ONE	2
+1	2	one	TWO	2
+2	3	two	THREE	2
+3	1	three	ONE	1
+EOF
+		'-g,mm'
+	],
+	[
+		[qw(-g 1,2 -m 1,field1,2,field2 -c -M t/map.yaml)], <<EOF,
+1	2	3
+1	2	4
+1	1	1
+2	3	1
+3	1	2
+1	1	2
+2	3	2
+EOF
+		<<EOF,
+1	1	one	ONE	2
+1	2	one	TWO	2
+2	3	two	THREE	2
+3	1	three	ONE	1
+EOF
+		'-g,mm'
+	],
+);
+
+plan tests => scalar @tests;
+check(@$_) for @tests;
